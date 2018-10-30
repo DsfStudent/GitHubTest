4aa3a723226ed77e58d23dcdb34f4a5ef72d7f95
dsferreira <dsferreira@H-A205-J8RZ482.nps.org>
2018-10-30 13:24:31 -0400

    First Commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author dsferreira
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // GitHub Hello World Test
+        String str = "Hello World!";
+        System.out.println(str);
+    }
+    
+}
